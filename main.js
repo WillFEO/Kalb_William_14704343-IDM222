9057f2f76036a4c59c59bef61ce3a2ccf32bcb85
William Kalb <wjk44@drexel.edu>
2026-03-17 13:36:00 -0400

    Addition to Previous Commit
    Forgot to hit save :(

--- main.js
+++ main.js
@@ -1,18 +1,6 @@
 const themeToggle = document.getElementById("theme-toggle");
-// const savedTheme = localStorage.getItem("theme");
 
-// // Restore theme preference from localStorage, or use system preference
-// if (savedTheme === "dark") {
-//   themeToggle.checked = true;
-// } else if (savedTheme === "light") {
-//   themeToggle.checked = false;
-// } else {
-//   // No saved preference, let system preference take over (don't add any class)
-//   themeToggle.checked = window.matchMedia(
-//     "(prefers-color-scheme: dark)",
-//   ).matches;
-// }
-
+// Set toggle to correct state based on dark or light mode
 themeToggle.checked = document.documentElement.classList.contains("dark");
 
 themeToggle.addEventListener("change", () => {
